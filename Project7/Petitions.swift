c9cd09cdbb9dad82d88654dc9663f439020b8be7
Dan <dburnett@frontiernet.net>
2019-03-05 22:53:08 -0500

    Day 33 - seems to parse

---
+++ Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Dan on 3/5/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
