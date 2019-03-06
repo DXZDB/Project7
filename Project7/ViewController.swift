63f9b95cc5e76417ae5e84815eaf7368e9a4e60d
Dan <dburnett@frontiernet.net>
2019-03-05 21:49:44 -0500

    Initial Commit

---
+++ Project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Dan on 3/5/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
